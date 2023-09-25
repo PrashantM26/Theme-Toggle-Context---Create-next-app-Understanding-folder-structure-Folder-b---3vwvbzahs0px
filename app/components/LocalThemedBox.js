48044d8e46a3a85e05d9cd1bae940cd35a54ed56
PrashantM26 <135729171+PrashantM26@users.noreply.github.com>
2023-09-25 20:51:50 +0530

    Update LocalThemedBox.js

--- app/components/LocalThemedBox.js
+++ app/components/LocalThemedBox.js
@@ -1,12 +1,28 @@
 'use client'
 import React from 'react';
-
+import { useTheme } from './components/ThemeProvider';
 
 const LocalThemedBox = () => {
 
+  const { theme, toggleTheme } = useTheme();
+  const toggleLocalTheme = () => {
+    toggleTheme();
+  };
+    
 return(
     <div style={{width:'200px',height:'200px',border:'2px solid green'}} id="local-themed-box">
         {/* Write code below this line */}
+      <p className={`txt-${theme}`} id="local-themed-text-container">
+        Some Text
+      </p>
+      <button
+        className={`btn btn-${theme}`}
+        id="local-theme-toggler"
+        onClick={toggleLocalTheme}>
+        {theme === 'light'
+          ? 'Toggle local theme to dark'
+          : 'Toggle local theme to light'}
+      </button>
         
     </div>
 )
