c249077f9ac0bc5ef9e9db134cceaf4b73446666
PrashantM26 <135729171+PrashantM26@users.noreply.github.com>
2023-10-04 10:59:39 +0530

    Update ThemeProvider.js

--- app/components/ThemeProvider.js
+++ app/components/ThemeProvider.js
@@ -1,11 +1,11 @@
-'use client'
+/*'use client'
 import React, { createContext, useState, useContext } from 'react';
 
 const ThemeContext = createContext()
 
 /*export const useTheme = () => {
   return useContext(ThemeContext);
-};*/
+};*
 
 
 const ThemeProvider = ({ children }) => {
@@ -24,7 +24,61 @@ const ThemeProvider = ({ children }) => {
     )
 }
 
-export {ThemeProvider,ThemeContext}
+export {ThemeProvider,ThemeContext}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"use client";
+import React, { useState } from "react";
+
+const initialTheme = { isLight: true };
+// this is called a hoc
+const ThemeContext = React.createContext(initialTheme);
+const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState(initialTheme);
+
+  return (
+    <ThemeContext.Provider value={[theme, setTheme]}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export { ThemeProvider, ThemeContext };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
