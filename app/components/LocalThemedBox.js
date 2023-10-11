ba82aa5148333cd8386d13c397335809c1b355ac
PrashantM26 <135729171+PrashantM26@users.noreply.github.com>
2023-10-11 19:55:44 +0530

    Update LocalThemedBox.js

--- app/components/LocalThemedBox.js
+++ app/components/LocalThemedBox.js
@@ -32,7 +32,12 @@ return(
 
 export { LocalThemedBox }*/
 
-"use client";
+
+
+
+
+
+/*"use client";
 import React, { useContext, useEffect, useState } from "react";
 import { ThemeContext } from "./ThemeProvider";
 
@@ -48,7 +53,7 @@ const LocalThemedBox = () => {
       className={`container ${"bg-" + (innerTheme.isLight ? "light" : "dark")}`}
       id="local-themed-box"
     >
-      {/* Write code below this line */}
+      {/* Write code below this line *}
       <p
         id="themed-text-container"
         className={`${"txt-" + (innerTheme.isLight ? "light" : "dark")}`}
@@ -69,4 +74,57 @@ const LocalThemedBox = () => {
   );
 };
 
-export { LocalThemedBox };
+export { LocalThemedBox };*/
+
+
+
+
+
+
+
+
+
+
+//Alok
+
+
+
+'use client'
+import React, { useContext, useEffect, useState } from 'react';
+import { ThemeContext } from './ThemeProvider';
+
+// export const LocalThemeContext = React.createContext({
+//     theme: {value : 0},
+//     setTheme : ()=>{}
+// })
+const LocalThemedBox = () => {
+
+    // const [theme,setTheme] = React.useState({value : 0});
+    
+    // function handleTheme(){
+    //     setTheme((oldTheme)=>{
+    //         const newTheme = {...oldTheme};
+    //         newTheme.value = newTheme.value === 0 ? 1 : 0;
+    //         return newTheme;
+    //     })
+    // }
+        const[value,setValue] = useState(true);
+        const{theme} = useContext(ThemeContext)
+        useEffect(()=>{
+            theme ? setValue(true) : setValue(false)
+        },[theme])
+return(
+    // <LocalThemeContext.Provider value={{theme: theme, setTheme: handleTheme}}>
+    <div className={value ? 'bg-light':'bg-dark'} style={{width:'200px',height:'200px',border:'2px solid green'}} id="local-themed-box">
+        {/* Write code below this line */}
+        <p id='local-themed-text-container' className={value ? 'txt-light':'txt-dark'}>Hii</p>
+        <button id='local-theme-toggler' className={`btn ${value ? "btn-light txt-light" : "btn-dark txt-dark"}`} onClick={()=>{
+            setValue(!value)
+        }}>Toggle local theme to {value ? 'dark' : "light"}</button>
+       
+    </div>
+    // </LocalThemeContext.Provider>
+)
+}
+
+export { LocalThemedBox }
